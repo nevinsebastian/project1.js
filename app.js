627acefc4e0df44e575fa137dd65237f3e555c78
nevin <nevinsebastian@cse.carmelcet.in>
2023-11-02 03:26:35 +0530

    js

---
+++ app.js
@@ -0,0 +1,35 @@
+//console.log("hello world"); // basic consol log
+//
+//// variable are boxes where we can place a value
+//
+//var life = 100; // here the life is a variable and has value of 100
+//
+//life = 50;// here we update the value js gose from to to down
+//life = life - 10; // also we can update like this
+//
+//var name = "Nevin Sebastian"; // now it is text string
+//
+//var checkout = false;
+//
+//var box; // box is undifined now
+//
+//var box = null; // now also null
+//
+//
+//console.log(box);
+
+
+// OLD TECH
+
+//NEV TECH ||
+    //     \/
+
+//const life = 100;
+//const life = 100; // const cant be changed  like var
+
+let life = 100
+
+life = life - 50; // but we can change let
+
+console.log(life);
+ 
