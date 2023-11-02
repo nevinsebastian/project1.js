b66eaa209cc058592d3528a0569633463c070217
nevin <nevinsebastian@cse.carmelcet.in>
2023-11-02 12:42:28 +0530

    dom manipulation

---
+++ tut.js
@@ -0,0 +1,130 @@
+//console.log("hello world"); // basic consol log
+//
+//// variable are boxes where we can place a value
+//
+//var life = 100; // here the life is a variable and has value of 100
+//
+//life = 50;// here we update the value js gose from to to down
+//life = life - 10; // also we can update like this
+//
+//var name = "Nevin Sebastian"; // now it is text string
+//
+//var checkout = false;
+//
+//var box; // box is undifined now
+//
+//var box = null; // now also null
+//
+//
+//console.log(box);
+
+
+// OLD TECH
+
+//NEV TECH ||
+    //     \/
+
+//const life = 100;
+//const life = 100; // const cant be changed  like var
+
+//let life = 100
+//
+//life = life - 50; // but we can change let
+//
+//console.log(life);
+ 
+
+
+//function logger() {
+//    console.log("Party Time");
+//    console.log("Party Time");
+//    console.log("Party Time");
+//    console.log("Party Time");
+//    console.log("Party Time");
+//    console.log("Party Time");
+//    }
+//
+//logger(name);
+//
+//function Adder(num1,num2) {
+//    console.log(num1 + num2);
+//}
+//
+//Adder(6,6);
+//const name = "Nevin Sebastian";
+//const youtuber = 'MR beast'
+//
+//function toUpper(text) {
+//    const upperCased = text.toUpperCase();
+//    console.log(upperCased);
+//}
+//toUpper(name);
+//
+//
+//const toUpper = () => {
+//    asbasd
+//}
+//const name = 'Nevin'
+//
+//console.log("my name is " + "Nevin") // string concatination
+//console.log(`my name is ${name}`)
+//
+
+//const dice1 = 6;
+//const dice2 = 3;
+//
+//if(dice1 === 6 || dice2 === 6){
+//    console.log('you rolled a double');
+//}else{
+//    console.log('Sorry you are not good to go');
+//}
+
+//const schedule = [
+//    'Wake up',
+//    'Eat ',
+//    'Get reddy',
+//    'Write code'
+//];
+
+///schedule.push('Push to gitHub'); add
+//schedule.pop()// delet from end
+//schedule.shift() // delet from first
+
+
+//const user = {
+//    name: 'Nevin',
+//    age: 22,
+//    married:false,
+//    purchases:["iPhone","Swift","Laptop"],
+//
+//    sayName : function() {
+//        console.log(this.name);
+//    }
+//}
+//
+//
+//user.sayName();
+//
+//function sayMyAge(){
+//    console.log(`My age is ${this}`);
+//}
+//sayMyAge();
+
+//const names = ['Nevin', 'Mishell','Binn','Jeevan','Adarsh','Ikka','PP'];
+//
+//for(name of names) {
+//    console.log(name);
+//    if(name === 'Mishell'){
+//        console.log("mishel is in my name");
+//        break;
+//    }
+//}
+
+//let loading = 0;
+//
+//while(loading < 100){
+//    console.log('website is still loading');
+//
+//    loading += 1;
+//}
+
