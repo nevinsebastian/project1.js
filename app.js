a96186604f1f1868965212c1904001ce3b18d904
nevin <nevinsebastian@cse.carmelcet.in>
2023-11-04 08:33:40 +0530

    dom manu

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-const userList = document.querySelectorAll('.name-list li');
+const userList = document.querySelector('.name-list li');
+const listInput = document.querySelector('.list-input');
+const addListBtn = document.querySelector('.addListBtn');
 
-console.log(userList);
+addListBtn.addEventListener('click',function(){
+    const newLi = document.createElement('LI');
+    const liContent = document.createTextNode(listInput.value)
+    newLi.appendChild(liContent);
+
+    userList.appendChild(newLi);S
+});
