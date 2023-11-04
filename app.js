c4d64d7d29ab817892d477bb63b298c628803a0e
nevin <nevinsebastian@cse.carmelcet.in>
2023-11-04 09:24:00 +0530

    smooth scroll

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-const userList = document.querySelector('.name-list li');
-const listInput = document.querySelector('.list-input');
-const addListBtn = document.querySelector('.addListBtn');
+function smoothScroll(target,duration){
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect().top;
+    var startingPosition = window.pageYOffset;
+    var distance = targetPosition - startingPosition;
+    var startTime= null;
 
-addListBtn.addEventListener('click',function(){
-    const newLi = document.createElement('LI');
-    const liContent = document.createTextNode(listInput.value)
-    newLi.appendChild(liContent);
+    function animation(currentTime){
+        if(startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed, startingPosition,distance,duration);
+        window.scrollTo(0,run);
+        if(timeElapsed < duration) requestAnimationFrame(animation);
+    }
 
-    userList.appendChild(newLi);S
+    function ease(t, b, c, d){
+        t /= d/2;
+        if (t < 1) return c/2*t*t + b;
+        t --;
+        return -c / 2 * (t *(t - 2) -1) + b;
+
+    }
+
+    requestAnimationFrame(animation);
+
+}
+
+var section1 = document.querySelector('.section1');
+
+section1.addEventListener('click',function(){
+    smoothScroll('.section2',1000);
 });
